49bfd052f066cc97c49ed914104aa1fbacddb9ea
Mickael Fernandes <fmickael291@gmail.com>
2023-05-07 15:47:53 -0300

    estrutura do main_client finalizada, funcionalidade de filtro finalizada

---
+++ front_end/controller/cadastro_prod.js
@@ -0,0 +1,7 @@
+let btnCancelar = document.getElementById("btnCancelar");
+
+btnCancelar.addEventListener("click", function (e) {
+  e.preventDefault();
+  window.location.href = "./main_admin.html";
+});
+
